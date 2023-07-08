20704ca91a265c8e4d733c5da20d3a75b99db75f
Victor Raul <v1c70rotakmer@gmail.com>
2023-07-07 21:05:48 -0500

    Terminar estructura HTML del proyecto

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Initialize Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
